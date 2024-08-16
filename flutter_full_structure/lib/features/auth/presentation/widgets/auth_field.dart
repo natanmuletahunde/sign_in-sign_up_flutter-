b4168b50b047e8688cd046cc20669c219f419bd3
pivnati <natanmuleta911@gmail.com>
2024-08-16 15:01:31 -0700

    adding the isobscure to all the inputs

--- flutter_full_structure/lib/features/auth/presentation/widgets/auth_field.dart
+++ flutter_full_structure/lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 class AuthField extends StatelessWidget {
+  final TextEditingController controller;
   final String hintext;
-  const AuthField ({super.key, required this.hintext});
-
+  final bool  isObscureText;
+  const AuthField ({super.key, required this.hintext,required this.controller,
+  this.isObscureText = false, required bool obscureText});
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+       controller: controller,
        decoration: InputDecoration(
         hintText: hintext
        ),
+       validator: (value){
+          if(value!.isEmpty){
+            return 'Please enter your $hintext';
+          }
+          return null;
+       },
+       obscureText: isObscureText,
     );
   }
 }
