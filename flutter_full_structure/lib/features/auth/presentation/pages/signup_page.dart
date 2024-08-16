691e269143a146662c068fc635d748a8b43b9523
pivnati <natanmuleta911@gmail.com>
2024-08-16 14:57:51 -0700

    adding the isobscure to all the inputs

--- flutter_full_structure/lib/features/auth/presentation/pages/signup_page.dart
+++ flutter_full_structure/lib/features/auth/presentation/pages/signup_page.dart
@@ -10,47 +10,65 @@ class SignUpPage extends StatefulWidget {
 }
 
 class  _SignUpPage extends State <SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+ final formtype = GlobalKey<FormState>(); 
+ @override
+  void dispose() { // used for cleaning uo the controllers when the page is diposed.
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
      body: Padding(
        padding: EdgeInsets.all(15),
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-       children:  [
-        Text('Sign Up.' , style: TextStyle(
-          fontSize: 50,
-          fontWeight: FontWeight.bold
-        ),),
-        SizedBox(
-          height: 30,
-        ),
-          AuthField(hintext: 'Name'),
-           SizedBox(
-          height: 15,
-        ),
-          AuthField(hintext: 'Email'),
-           SizedBox(
-          height: 15,
-        ),
-          AuthField(hintext: 'Password'),
-           SizedBox(
-          height: 15,
-        ),
-        SizedBox(
-          height: 20,
-        ),AuthGradientButton(),
-        SizedBox(
-          height: 15,
-        ),
-         RichText(text: TextSpan(text: "don't have an account?", style: Theme.of(context).textTheme.titleMedium,
-         children: [
-            TextSpan(
-              text: 'sign in',
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(color:AppPallete.gradient2,)
-            )
-         ]))
-       ],
+       child: Form(
+          key: formtype,
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+         children:  [
+           const Text('Sign Up.' , style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold
+          ),),
+           const SizedBox(
+            height: 30,
+          ),
+              AuthField(hintext: 'Name',
+             controller:nameController, obscureText: true,),
+             const SizedBox(
+            height: 15,
+          ),
+             AuthField(hintext: 'Email',
+            controller: emailController, obscureText: true,),
+              const SizedBox(
+            height: 15,
+          ),
+              AuthField(hintext: 'Password',
+              obscureText: true,
+              controller: passwordController,),
+              const SizedBox(
+            height: 15,
+          ),
+           const SizedBox(
+            height: 20,
+          ),const AuthGradientButton(),
+          const  SizedBox(
+            height: 15,
+          ),
+            RichText(text: TextSpan(text: "don't have an account?", style: Theme.of(context).textTheme.titleMedium,
+           children: [
+              TextSpan(
+                text: 'sign in',
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(color:AppPallete.gradient2,)
+              )
+           ]))
+         ],
+         ),
        ),
      ),
     );
