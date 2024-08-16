6b432f1e766102f87c13c87f15c8997ea11aee90
pivnati <natanmuleta911@gmail.com>
2024-08-16 15:33:54 -0700

    adding the GestureDetector   richtext  sign in

--- flutter_full_structure/lib/features/auth/presentation/pages/login_page.dart
+++ flutter_full_structure/lib/features/auth/presentation/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_full_structure/core/theme/app_pallet.dart';
+import 'package:flutter_full_structure/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter_full_structure/features/auth/presentation/widgets/auth_field.dart';
 import 'package:flutter_full_structure/features/auth/presentation/widgets/auth_gradient.dart';
 
@@ -30,7 +32,7 @@ class _LoginPage extends State<LoginPage> {
 
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(15),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -56,20 +58,27 @@ class _LoginPage extends State<LoginPage> {
               ),
               const SizedBox(height: 15),
               const SizedBox(height: 20),
-              const AuthGradientButton(),
+              const AuthGradientButton(
+                buttonText: 'sign in',
+              ),
               const SizedBox(height: 15),
-              RichText(
-                text: TextSpan(
-                  text: "Don't have an account? ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      text: 'Sign up',
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: AppPallete.gradient2,
-                          ),
-                    ),
-                  ],
+              GestureDetector(
+                onTap: () {
+                   Navigator.push(context, MaterialPageRoute(builder:(context) => const SignUpPage()));
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don't have an account? ",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Sign up',
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                              color: AppPallete.gradient2,
+                            ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               SizedBox(height: keyboardHeight), // Adjust padding based on keyboard height
